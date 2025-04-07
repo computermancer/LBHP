940620396cd4130041d2918a197b0c0be6488e01
User <user@example.com>
2025-04-06 20:16:06 -0700

    yup

--- components/Nav.js
+++ components/Nav.js
@@ -1,7 +1,7 @@
 import Link from 'next/link';
 export default function Nav() {
   return (
-    <div className="sticky top-0 z-50 bg-zinc-900 shadow-md">
+    <div className="sticky top-0 z-50 bg-zinc-00 shadow-md">
       <div className="px-8">
         <nav className="flex gap-0.5 text-gray-200 py-2 max-w-[500px] mx-auto">
           <Link className="flex-1 bg-zinc-800 px-1.5 py-2 rounded text-base font-semibold hover:bg-zinc-700 text-center whitespace-nowrap" href="/home">Home</Link>
