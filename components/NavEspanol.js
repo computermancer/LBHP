3e3d1bfb808c09206c5bbcd78d10b2e7c228b7a6
User <user@example.com>
2025-04-06 17:47:31 -0700

    nav bar initial

--- components/NavEspanol.js
+++ components/NavEspanol.js
@@ -4,12 +4,12 @@ export default function NavEspanol() {
     <div className="sticky top-0 z-50 bg-zinc-900 shadow-md">
       <div className="px-8">
         <nav className="flex gap-0.5 text-gray-200 py-2 max-w-[500px] mx-auto">
-          <Link className="flex-1 bg-zinc-800 px-1.5 py-2 rounded text-xl font-semibold hover:bg-zinc-700 text-center " href="/espanol/inicio">Inicio</Link>
-          <Link className="flex-1 bg-zinc-800 px-1.5 py-2 rounded text-xl font-semibold hover:bg-zinc-700 text-center " href="/espanol/semana1">Semana 1</Link>
-          <Link className="flex-1 bg-zinc-800 px-1.5 py-2 rounded text-xl font-semibold hover:bg-zinc-700 text-center " href="/espanol/semana2">Semana 2</Link>
-          <Link className="flex-1 bg-zinc-800 px-1.5 py-2 rounded text-xl font-semibold hover:bg-zinc-700 text-center " href="/espanol/semana3">Semana 3</Link>
-          <Link className="flex-1 bg-zinc-800 px-1.5 py-2 rounded text-xl font-semibold hover:bg-zinc-700 text-center " href="/espanol/semana4">Semana 4</Link>
-          <Link className="flex-1 bg-zinc-800 px-1.5 py-2 rounded text-xl font-semibold hover:bg-zinc-700 text-center " href="/espanol/semana5">Semana 5</Link>
+          <Link className="flex-1 bg-zinc-800 px-1.5 py-2 rounded text-base font-semibold hover:bg-zinc-700 text-center whitespace-nowrap" href="/espanol/inicio">Inicio</Link>
+          <Link className="flex-1 bg-zinc-800 px-1.5 py-2 rounded text-base font-semibold hover:bg-zinc-700 text-center whitespace-nowrap" href="/espanol/semana1">S 1</Link>
+          <Link className="flex-1 bg-zinc-800 px-1.5 py-2 rounded text-base font-semibold hover:bg-zinc-700 text-center whitespace-nowrap" href="/espanol/semana2">S 2</Link>
+          <Link className="flex-1 bg-zinc-800 px-1.5 py-2 rounded text-base font-semibold hover:bg-zinc-700 text-center whitespace-nowrap" href="/espanol/semana3">S 3</Link>
+          <Link className="flex-1 bg-zinc-800 px-1.5 py-2 rounded text-base font-semibold hover:bg-zinc-700 text-center whitespace-nowrap" href="/espanol/semana4">S 4</Link>
+          <Link className="flex-1 bg-zinc-800 px-1.5 py-2 rounded text-base font-semibold hover:bg-zinc-700 text-center whitespace-nowrap" href="/espanol/semana5">S 5</Link>
         </nav>
       </div>
     </div>
