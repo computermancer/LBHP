9ba2986adf356aa6cc209c9d1c545e0b64726c7a
computermancer <bravo.mateo@gmail.com>
2025-04-13 12:29:17 -0700

    icons

--- components/icono.js
+++ components/icono.js
@@ -4,19 +4,19 @@ import Link from 'next/link';
 export default function Icono() {
   return (
     <div className="flex justify-center space-x-8">
-      <a href="https://www.mateobravo.com" target="_blank" rel="noopener noreferrer" className="text-white hover:text-orange-300">
+      <a href="https://www.mateobravo.com" target="_blank" rel="noopener noreferrer" className="text-gray-300 hover:text-gray-100">
         <FaGlobe className="text-3xl" />
       </a>
-      <a href="https://www.facebook.com/MateoBravoPersonalTraining" target="_blank" rel="noopener noreferrer" className="text-[#1877F2] hover:text-orange-300">
+      <a href="https://www.facebook.com/MateoBravoPersonalTraining" target="_blank" rel="noopener noreferrer" className="text-[#1877F2] hover:text-[#4D90FE]">
         <FaFacebook className="text-3xl" />
       </a>
-      <a href="https://www.youtube.com/@MateoBravoPersonalTraining" target="_blank" rel="noopener noreferrer" className="text-[#FF0000] hover:text-orange-300">
+      <a href="https://www.youtube.com/@MateoBravoPersonalTraining" target="_blank" rel="noopener noreferrer" className="text-[#FF0000] hover:text-[#FF4444]">
         <FaYoutube className="text-3xl" />
       </a>
-      <a href="https://www.instagram.com/flatfootmatt" target="_blank" rel="noopener noreferrer" className="text-[#E4405F] hover:text-orange-300">
+      <a href="https://www.instagram.com/flatfootmatt" target="_blank" rel="noopener noreferrer" className="text-[#E4405F] hover:text-[#FF6980]">
         <FaInstagram className="text-3xl" />
       </a>
-      <Link href="/espanol/comentarios" className="text-white hover:text-orange-300">
+      <Link href="/espanol/comentarios" className="text-gray-300 hover:text-gray-100">
         <FaEnvelope className="text-3xl" />
       </Link>
     </div>
