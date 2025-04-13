a22f8ca279bdb56a86b33edb50ba3593ead16698
computermancer <bravo.mateo@gmail.com>
2025-04-13 11:48:10 -0700

    padding

--- components/MainEspNavBar.js
+++ components/MainEspNavBar.js
@@ -2,7 +2,7 @@ import Link from 'next/link';
 
 export default function MainEspNavBar() {
   return (
-    <div className="sticky top-0 bg-zinc-900 pt-4">
+    <div className="fixed top-0 left-0 right-0 bg-zinc-900 pt-4 z-50">
       <div className="relative bottom-0 left-0 right-0">
         <div className="px-8">
           <nav className="flex gap-0.5 text-gray-200 py-0 max-w-[500px] mx-auto">
